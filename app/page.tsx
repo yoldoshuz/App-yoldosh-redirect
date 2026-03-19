b6f7afe80d7b48fa6d74e615ee035901bd9a1034
yoldoshuz <admin@yoldosh.uz>
2026-03-19 18:18:07 +0500

    fix: page

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,13 @@
-'use client';
-
-import { useEffect } from 'react';
-
 export default function Page() {
-  useEffect(() => {
-    const ua = navigator.userAgent;
-
-    const isAndroid = /Android/i.test(ua);
-    const isIOS = /iPhone|iPad|iPod/i.test(ua);
-
-    // ВСТАВЬ СВОИ ССЫЛКИ
-    const ANDROID_URL = 'https://play.google.com/store/apps/details?id=com.yoldosh.android';
-    const IOS_URL = 'https://apps.apple.com/uz/app/yoldosh/id6754510387';
-
-    if (isAndroid) {
-      window.location.href = ANDROID_URL;
-    } else if (isIOS) {
-      window.location.href = IOS_URL;
-    }
-  }, []);
-
-  return <div>Redirecting...</div>;
+  return (
+    <div style={{
+      height: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      fontSize: '20px'
+    }}>
+      Device is not supported
+    </div>
+  );
 }
